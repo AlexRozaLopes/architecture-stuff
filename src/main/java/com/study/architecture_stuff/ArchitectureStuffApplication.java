90cd3e15c55e7ddbb6406279d2ebae44c0ec4a01
lopes <alexrozaestudos@gmail.com>
2025-10-21 20:47:14 -0300

    iniciando projeto

---
+++ src/main/java/com/study/architecture_stuff/ArchitectureStuffApplication.java
@@ -0,0 +1,13 @@
+package com.study.architecture_stuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureStuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/architecture_stuff/ArchitectureStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.architecture_stuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
